e3363acfc0a9a6158119946814699ebca2182647
Lambda Dusk <lambdadusk@gmail.com>
2015-04-23 18:52:22 +0000

    More rest methods for User

--- app/model/plugin/slug.js
+++ app/model/plugin/slug.js
@@ -1,22 +1,16 @@
-var mongoose,
-    __indexOf = [].indexOf || function(item) {
-        for (var i = 0, l = this.length; i < l; i++) {
-          if (i in this && this[i] === item) return i;
-        }
-        return -1;
-    };
-
-mongoose = require('mongoose');
-
 module.exports = function(schema, options) {
-    var field, mkslug, model, modelName, reserved;
-    if (options == null) {
-      options = {};
-    }
-    field = options.field || 'name';
-    reserved = options.reserved || ['new', 'edit'];
-    modelName = options.model;
-    model = null;
+  "use strict";
+  
+  if (options == null) {
+    options = {};
+  }
+
+  var mongoose = require('mongoose'),
+    field = options.field || 'name',
+    reserved = options.reserved || ['new', 'edit'],
+    modelName = options.model,
+    model = null,
+
     mkslug = function(name) {
       if (options.slugify && typeof options.slugify === 'function') {
         return options.slugify(name);
@@ -24,53 +18,70 @@ module.exports = function(schema, options) {
       else {
         return name.parameterize();
       }
-    };
-    schema.add({
-      _slugs: [String]
-    });
-    schema.index({
-      _slugs: 1
-    }, {
-      unique: true,
-      sparse: true
-    });
-    schema.pre('save', function(done) {
-      var slug;
-      slug = mkslug(this[field]);
-      if (__indexOf.call(this._slugs, slug) >= 0) {
-        return done();
+    },
+
+    __indexOf = [].indexOf || function(item) {
+      for (var i = 0, l = this.length; i < l; i++) {
+        if (i in this && this[i] === item) return i;
       }
-      else {
-        if (!model) {
-          model = mongoose.model(modelName);
-        }
-        if (!model) {
-          throw new Error('Modelname is required!');
-        }
-        return model.count({
-          _slugs: new RegExp("^" + slug)
-        }, (function(_this) {
-          return function(err, cnt) {
-            if (cnt > 0) {
-              slug += "-" + cnt;
-            }
-            if (_this._slugs) {
-              _this._slugs.unshift(slug);
-            }
-            else {
-              _this._slugs = [slug];
-            }
-            return done();
-          };
-        })(this));
-      }
-    });
-    (schema.virtual('slug')).get(function() {
-      return this._slugs[0];
-    });
-    return schema.statics.findBySlug = function(slug, done) {
-      return this.findOne({
-        _slugs: slug
-      }, done);
+      return -1;
     };
+
+
+  schema.add({
+    _slugs: [String]
+  });
+
+  schema.index({
+    _slugs: 1
+  }, {
+    unique: true,
+    sparse: true
+  });
+
+  schema.pre('save', function(done) {
+    var slug = mkslug(this[field]);
+    
+    if (__indexOf.call(this._slugs, slug) >= 0) {
+      return done();
+    } else {
+      if (!model) {
+        model = mongoose.model(modelName);
+      }
+      
+      if (!model) {
+        throw new Error('Modelname is required!');
+      }
+      
+      model.count({
+        _slugs: new RegExp("^" + slug)
+      }, (function(_this) {
+        return function(err, cnt) {
+          if(err) return done(err);
+          
+          if (cnt > 0) {
+            slug += "-" + cnt;
+          }
+          if (_this._slugs) {
+            _this._slugs.unshift(slug);
+          }
+          else {
+            _this._slugs = [slug];
+          }
+          
+          done();
+        };
+      })(this));
+    }
+  });
+  
+  schema.virtual('slug').get(function() {
+    return this._slugs[0];
+  });
+  
+  schema.statics.findBySlug = function(slug, done) {
+    return this.findOne({
+      _slugs: slug
+    }, done);
+  };
 };
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,92 +1,202 @@
-module.exports = function (app) {
+module.exports = function(app) {
     "use strict";
 
     var User = require('../model').User;
     var ObjectId = require('mongoose').Schema.Types.ObjectId;
     var _ = require('lodash');
-    
-    var loadUser = function (req, res, next) {
-        if(!req.user && req.user.id) {
+
+    var loadUser = function(req, res, next) {
+        if (!req.user && req.user.id) {
             return next("missing user id");
         }
-        
+
         User.findOne({
             _id: req.user.id
-        }, function (err, user) {
-            if(err) return next(err);
-            if(!user) {
-                res.status(404).send({error: 'user does not exist'});
+        }, function(err, user) {
+            if (err) return next(err);
+            if (!user) {
+                res.status(404).send({
+                    error: 'user does not exist'
+                });
                 return;
             }
-            
+
             req.user = user;
             next();
         });
     };
-    
-    app.get('/users', app.oauth.authorise(), loadUser, function (req, res, next) {
-        if(req.user.role !== 'admin' && req.user.role !== 'moderator') {
-            res.status(403).send({error: 'Forbidden'});
+
+    app.get('/users', app.oauth.authorise(), loadUser, function(req, res, next) {
+        if (req.user.role !== 'admin' && req.user.role !== 'moderator') {
+            res.status(403).send({
+                error: 'Forbidden'
+            });
             return;
         }
-        
+
         req.checkQuery('limit', 'Invalid limit').optional().isInt();
         req.checkQuery('offset', 'Invalid offset').optional().isInt();
-        
+
         var limit = req.query.limit || 10;
         var offset = req.query.offset || 0;
-        
+
         // TODO: Add sorting options
-        
+
         limit = Math.max(Math.min(limit, 25), 1);
-        
+
         var errors = req.validationErrors();
-        if(errors) {
+        if (errors) {
             res.status(400).send(errors);
             return;
         }
-        
-        User.find({}, 'name _slugs').sort({'name': 1})
+
+        User.find({}, 'name _slugs').sort({
+                'name': 1
+            })
             .skip(offset)
             .limit(limit)
-            .exec(function (err, users) {
-            if(err) return next(err);
-            
-            res.send(_.map(users, function(user) {
-                return {
-                    slug: user.slug,
-                    name: user.name
-                }
-            }));
-        });
+            .exec(function(err, users) {
+                if (err) return next(err);
+
+                res.send(_.map(users, function(user) {
+                    return {
+                        slug: user.slug,
+                        name: user.name
+                    }
+                }));
+            });
     });
 
-    app.post('/users', function (req, res) {
+    app.post('/users', function(req, res) {
         req.checkBody('email', 'Invalid e-Mail').notEmpty().isEmail();
         req.checkBody('password', 'Invalid Password').notEmpty().isLength(6);
         req.checkBody('name', 'Invalid name').notEmpty().isLength(4);
 
         var errors = req.validationErrors();
-        if(errors) {
+        if (errors) {
             res.status(400).send(errors);
-        } else {
+        }
+        else {
             var email = req.body.email,
                 password = req.body.password,
                 name = req.body.name;
 
-            User.register(email, password, name, function (err, user) {
-                if(err) {
-                    if(err.code === 11000) {
-                        res.status(400).send({error: 'Name or email already exist'});
-                    } else {
+            User.register(email, password, name, function(err, user) {
+                if (err) {
+                    if (err.code === 11000) {
+                        res.status(400).send({
+                            error: 'Name or email already exist'
+                        });
+                    }
+                    else {
                         res.status(500).send(err);
                     }
-                } else {
-                    res.send({success:true});
+                }
+                else {
+                    res.send({
+                        success: true
+                    });
                 }
             });
         }
     });
+
+    app.get('/user/:slug', app.oauth.authorise(), loadUser, function(req, res, next) {
+        var slug = req.params.slug;
+
+        User.findBySlug(slug, function(err, user) {
+            if (err) return next(err);
+            if (!user) {
+                res.status(404).send({
+                    error: 'user does not exist'
+                });
+            }
+
+            var result = {
+                name: user.name,
+                slug: user.slug
+            };
+
+            if (req.user.id === user.id) {
+                result.email = user.email;
+                result.created = user.created;
+            }
+
+            res.send(result);
+        });
+    });
+
+    app.put('/user/:slug', app.oauth.authorise(), loadUser, function(req, res, next) {
+            var slug = req.params.slug;
+
+            User.findBySlug(slug, function(err, user) {
+                if (err) return next(err);
+                if (!user) {
+                    res.status(404).send({
+                        error: 'user does not exist'
+                    });
+                }
+
+                if (user.id !== req.user.id && req.user.role !== 'admin') {
+                    res.status(403).send({
+                        error: 'Forbidden'
+                    });
+                    return;
+                }
+                
+                req.checkBody('email', 'Invalid e-Mail').optional().isEmail();
+                req.checkBody('password', 'Invalid Password').optional().isLength(6);
+                req.checkBody('name', 'Invalid name').optional().isLength(4);
+                
+                var errors = req.validationErrors();
+                if(errors) {
+                    res.status(400).send(errors);
+                    return;
+                }
+                
+                user.email = req.body.email || user.email;
+                user.password = req.body.password || user.password;
+                user.name = req.body.name || user.name;
+                
+                user.save(function (err) {
+                    if(err) {
+                        if(err.code === 11000) {
+                            res.status(400).send({error: 'Duplicate email or name'});
+                            return;
+                        }
+                        return next(err);
+                    }
+                    
+                    res.send({success: true});
+                });
+            });
+    });
     
-    
+    app.delete('/user/:slug', app.oauth.authorise(), loadUser, function(req, res, next) {
+            var slug = req.params.slug;
+
+            User.findBySlug(slug, function(err, user) {
+                if (err) return next(err);
+                if (!user) {
+                    res.status(404).send({
+                        error: 'user does not exist'
+                    });
+                }
+
+                if (user.id !== req.user.id && req.user.role !== 'admin') {
+                    res.status(403).send({
+                        error: 'Forbidden'
+                    });
+                    return;
+                }
+                
+                User.remove({_id: user.id}, function (err) {
+                    if(err) return res.err;
+
+                    // TODO: Remove this user's access/refresh tokens
+                    
+                    res.send({success: true});
+                });
+            });
+    });
 };
